da4904a6f849a39c05392e467f62a035f72ab095
Rijk van Zanten <rijkvanzanten@me.com>
2024-11-23 18:38:52 -0500

    Setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  future: {
-    compatibilityVersion: 4,
-  },
-  devtools: { enabled: true }
-})
+	compatibilityDate: "2024-11-01",
+
+	future: {
+		compatibilityVersion: 4,
+	},
+
+	devtools: { enabled: true },
+	modules: ["@nuxt/eslint"],
+});
