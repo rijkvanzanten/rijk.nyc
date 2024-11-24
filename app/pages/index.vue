e5b82cacdf2aaf68f775389739647f0d1d95e29d
Rijk van Zanten <rijkvanzanten@me.com>
2024-11-24 11:07:28 -0500

    Setup fonts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-	<h1>Hello World</h1>
+	<SiteHeader />
 </template>
+
+<style scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-11-01",
+	modules: ['@nuxt/eslint', '@nuxt/fonts'],
+	devtools: { enabled: true },
+
+	css: ['~/assets/css/main.css'],
 
 	future: {
 		compatibilityVersion: 4,
 	},
-
-	devtools: { enabled: true },
-	modules: ["@nuxt/eslint"],
+	compatibilityDate: '2024-11-01',
 
 	eslint: {
 		config: {
 			stylistic: {
-				indent: "tab",
+				indent: 'tab',
 				semi: true,
 			},
+		},
+	},
+
+	fonts: {
+		defaults: {
+			weights: [400, 700],
 		},
 	},
 });
