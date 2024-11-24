a07c70e109c5b08cb99aab8bc85b058c651fdd2c
Rijk van Zanten <rijkvanzanten@me.com>
2024-11-24 11:07:44 -0500

    Experiment with component / fonts

---
+++ app/components/SiteHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<header>
+		<h1>Rijk <strong>van Zanten</strong></h1>
+	</header>
+</template>
+
+<style scoped>
+header h1 {
+	font-weight: 400;
+}
+
+header strong {
+	font-weight: 800;
+}
+</style>
