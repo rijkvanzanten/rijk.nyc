02a6d6855887e076a5d965fd351ade95c184766a
Rijk van Zanten <rijkvanzanten@me.com>
2024-11-29 15:00:10 -0500

    Format config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/content'],
-				devtools: { enabled: true },
+	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/content'],
+	devtools: { enabled: true },
 
-				css: ['~/assets/css/main.css'],
+	css: ['~/assets/css/main.css'],
 
-				future: {
-								compatibilityVersion: 4,
-				},
-				compatibilityDate: '2024-11-01',
+	future: {
+		compatibilityVersion: 4,
+	},
+	compatibilityDate: '2024-11-01',
 
-				eslint: {
-								config: {
-												stylistic: {
-																indent: 'tab',
-																semi: true,
-												},
-								},
-				},
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+				semi: true,
+			},
+		},
+	},
 
-				fonts: {
-								defaults: {
-												weights: [400, 700],
-								},
-				},
+	fonts: {
+		defaults: {
+			weights: [400, 700],
+		},
+	},
 });
